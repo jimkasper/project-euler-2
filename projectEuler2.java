454251bf1edf543eb82bbedf831d9d5637a5eaf2
Jim Kasper <jimkasper@Jims-MacBook-Pro.local>
2015-09-30 15:46:20 -0400

    gottt it

--- projectEuler2.java
+++ projectEuler2.java
@@ -1,8 +1,23 @@
 public class projectEuler2 {
 	public static void main(String[] args) {
 		
+	int a = 1;
+	int b = 2;
+	int sum = 2;
+
+		for(int c = a+b; c <= 4000000; c = a+b){
+			
+			if(c%2 == 0){ 
+				sum = sum + c;
+			}		
+			
+			a = b;
+			b = c;
+		}	
+			
+	System.out.println("Total sum of even numbers: "+sum);
+	
 		
-		
-		
+	
 	}
 }
